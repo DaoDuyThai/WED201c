834643f10d6289cf62efdb7a23404037ad0f83ec
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-04-26 22:19:28 +0700

    add more question

---
+++ Spring 2024 - Classify platform/3. Result/index.js
@@ -0,0 +1,13 @@
+const categoryContainer = document.querySelector('.category-container');
+
+function displayCategory(){
+    categoryContainer.style.display = 'block';
+}
+
+function hideCategory(){
+    categoryContainer.style.display = 'none';
+}
+
+function aleartMessage(){
+    alert('Dit me may');
+}
